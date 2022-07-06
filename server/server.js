d3141277695d29f58c2c62ec9be7100d5f6c2377
d00434525 <d00434525@dmail.dixie.edu>
2022-07-06 09:54:41 -0600

    added some threads

--- server/server.js
+++ server/server.js
@@ -8,8 +8,9 @@ const app = express();
 // tell the server to understand how to read json
 app.use(express.json());
 
-setUpAuth(app);
 setUpSession(app);
+setUpAuth(app);
+
 
 // allow serving of UI code
 app.use(express.static(`${__dirname}/../public/`));
@@ -33,7 +34,7 @@ app.post("/users", async (req,res) => {
 app.post("/thread", async(req,res) => {
     // auth
     if (!req.user) {
-        res.status(401).json({ message: "unauthed" });
+        res.status(401).json({ message: "unauthorized." });
         return;
     }
     // create with await + try/catch
@@ -53,13 +54,64 @@ app.post("/thread", async(req,res) => {
     }
 });
 
-app.get("/thread/:id", (req,res) => {
-    
+app.get("/thread/:id", async (req,res) => {
+    // check authorization
+    if (!req.user) {
+        res.status(401).json({ message: "unauthorized."});
+        return;
+    }
+    try {
+        let thread = await Thread.findById(id);
+        req.status(201).json(thread);
+    } catch (error) {
+        res.status(500).json({
+            message: "could not get thread",
+            error: error,
+        });
+    }
 })
 
-app.get("/thread", (req,res) => {})
+app.get("/thread", async (req,res) => {
+    try {
+        let threads = await Thread.find({}, "-posts");
+        req.status(201).json(threads);
+    } catch (error) {
+        res.status(500).json({
+            message: "could not get threads",
+            error: error,
+        });
+    }
+    // get all users for all the threads
+    for (let k in threads) {
+        try {
+            threads[k] = threads[k].toObject();
+            let user = await User.findById(threads[k].user_id);
+            threads[k].user = user;
+        } catch (error) {
+            console.log(
+                `unable to get user ${threads[k].user_id} when getting thread ${threads[k]._id}: ${error}`
+            );
+        }
+    }
+    res.send(JSON.stringify(threads));
+});
 
-app.delete("/thread/:id", (req,res) => {})
+app.delete("/thread/:id", async (req,res) => {
+    // check authorization.
+    if (!req.user){
+        res.status(401).json({ message: "unauthorizaed." });
+        return;
+    }
+    try {
+        let thread = await Thread.findByIdAndDelete(id);
+        req.status(201).json(thread);
+    } catch {
+        res.status(500).json({
+            message: "could not delete thread.",
+            error: error,
+        });
+    }
+})
 
 app.post("/post", (req,res) => {})
 
